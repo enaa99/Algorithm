9e86315581ae5af9c2d6577fab2226e29a1177da
enaa99 <wprkfdlsgk@gmail.com>
2022-12-27 13:17:21 +0900

    dp coding

---
+++ baekjoonPython/DP/11057오르막 수.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+num = [1 for i in range(10)]
+
+
+for i in range(N-1):
+    for j in range(1,10):
+        num[j] = (num[j-1] +num[j])
+
+print(sum(num)%10007)
+
+

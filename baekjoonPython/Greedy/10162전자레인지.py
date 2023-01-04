3657e32207792b027e2d27111c3cfda35a86b6cc
enaa99 <wprkfdlsgk@gmail.com>
2023-01-04 11:48:19 +0900

    greedy

---
+++ baekjoonPython/Greedy/10162전자레인지.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+# A - 300 / B - 60 / C - 10
+
+
+A = 300
+B = 60
+C = 10
+
+
+if N%C != 0:
+    print(-1)
+    exit(0)
+
+
+k = N//A
+print(k,end=' ')
+
+N = N%A
+k = N//B
+print(k,end = ' ')
+
+N = N%B
+print(N//C)

fde3bc6d288b9b00486b2c318d611145fff64ded
enaa99 <wprkfdlsgk@gmail.com>
2023-01-12 13:51:27 +0900

    simulation

--- baekjoonPython/Greedy/마법의엘리베이터.py
+++ baekjoonPython/Greedy/마법의엘리베이터.py
@@ -1,27 +1,20 @@
 def solution(storey):
     answer = 0
-    # 0 층으로 가기
-    # 1억 그리디?
-    # 항상 10기준 가까운 곳으로 가기
-    
-    store = str(storey)
-    tmp = []
-    for i in range(len(store)-1,-1,-1):
-        tmp.append(int(store[i]))
-    
-    cnt = 0
-    for num in range(len(tmp)):
-        if 5 >= tmp[num]:
-            cnt += tmp[num]
-            
-            tmp[num] = 0
-            
-        else:
-            cnt += 10 - tmp[num]
-            if num != len(tmp)-1:
-                tmp[num+1] +=1
-            tmp[num] = 0            
-    
-    return tmp
 
-solution(16)
+    while storey:
+        remainder = storey % 10
+        # 6 ~ 9
+        if remainder > 5:
+            answer += (10 - remainder)
+            storey += 10
+        # 0 ~ 4
+        elif remainder < 5:
+            answer += remainder
+        # 5
+        else:
+            if (storey // 10) % 10 > 4:
+                storey += 10
+            answer += remainder
+        storey //= 10
+
+    return answer

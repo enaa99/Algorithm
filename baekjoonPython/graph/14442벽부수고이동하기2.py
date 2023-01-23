abed7a658f279c8df38d34d95cb4b700f86b0a8e
enaa99 <wprkfdlsgk@gmail.com>
2023-01-23 12:14:16 +0900

    graph

--- baekjoonPython/graph/14442벽부수고이동하기2.py
+++ baekjoonPython/graph/14442벽부수고이동하기2.py
@@ -1,33 +1,30 @@
-import sys
-input = sys.stdin.readline
 from collections import deque
+q = deque()
+from sys import stdin
+input = stdin.readline
 
-def bfs(x,y):
-    queue = deque()
-    queue.append([x,y,0])
-    visited = [[[0]*(K+1) for _ in range(M)] for _ in range(N)]
-    visited[x][y][0] = 1
-    while queue:
-        x,y,b_cnt = queue.popleft()
-        if x==N-1 and y==M-1:
-            return visited[x][y][b_cnt]
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx<N and 0<=ny<M:
-                if not visited[nx][ny][b_cnt]:
-                    if maps[nx][ny] and b_cnt < K:
-                        visited[nx][ny][b_cnt+1] = visited[x][y][b_cnt]+1
-                        queue.append([nx,ny,b_cnt+1])
-                    elif not maps[nx][ny]:
-                        visited[nx][ny][b_cnt] = visited[x][y][b_cnt]+1
-                        queue.append([nx,ny,b_cnt])
+n,m,k = map(int, input().split())
+vis = [[[0]*(k+1) for _ in range(m)] for __ in range(n)]
+arr = [list(map(int,input().strip())) for _ in range(n)]
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
 
+def bfs() :
+    q.append([0,0,k]) # k는 벽을 뚫을 수 있는 수
+    vis[0][0][k] = 1
+    while q :
+        x,y,z = q.popleft()
+        if x == n-1 and y == m-1 :
+            return vis[x][y][z]
+        for i in range(4) :
+            nx ,ny = dx[i] + x, dy[i]+y
+            if 0<=nx<n and 0<=ny<m :
+                if arr[nx][ny]==1 and z>0 and vis[nx][ny][z-1]==0:
+                    vis[nx][ny][z-1] = vis[x][y][z]+1
+                    q.append([nx,ny,z-1])
+                elif arr[nx][ny]==0 and vis[nx][ny][z]==0:
+                    vis[nx][ny][z] = vis[x][y][z]+1
+                    q.append([nx,ny,z]) 
     return -1
 
-N, M, K = map(int, input().split())
-maps = [list(map(int, list(input().rstrip()))) for _ in range(N)]
-
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-print(bfs(0,0))
+print(bfs())

b7cfc0139a6af5825ae165f77d81e6099edefc7e
enaa99 <wprkfdlsgk@gmail.com>
2023-02-11 16:14:47 +0900

    max

---
+++ baekjoonPython/simulation/2566.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+
+answer = [-1,0,0]
+for i in range(9):
+    
+    l = list(map(int,input().split()))
+    
+    max_v = max(l)
+    k = [max_v,i+1,l.index(max_v)+1]
+    
+    if answer[0] < k[0]:
+        answer[0],answer[1],answer[2] = k[0],k[1],k[2]
+    
+
+print(answer[1],answer[2])

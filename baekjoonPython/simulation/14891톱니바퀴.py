e98434292d31a791b9adefa9c1d296c5c2a5049b
enaa99 <wprkfdlsgk@gmail.com>
2023-03-11 21:45:24 +0900

    simulation

--- baekjoonPython/simulation/14891톱니바퀴.py
+++ baekjoonPython/simulation/14891톱니바퀴.py
@@ -1,28 +1,48 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
+t = [deque(list(map(int,input().rstrip()))) for _ in range(4)] # 톱니 상태 저장
 
-graph = []
-
-
-for _ in range(4):
+# ✨ 톱니 돌리기 
+k = int(input())
+for _ in range(k): 
+	r = [] # 처음 톱니 상태 저장
+	for i in range(4):
+		r.append([t[i][6],t[i][2]])
+	n,d = map(int,input().split())
+	n = n-1
     
-    graph.append(list(deque(int,input().split())))
-
-K = int(input())
-
-# 시계방향 왼쪽으로 한칸 / 반시계방향 오른쪽으로 한칸
-# N 극 0, S극 1
-
-
-for _ in range(K):
-    a,b = map(int,input().split())
+    # 왼쪽에 있는 톱니들 돌리기
+	if n != 0 :
+		for i in range(n,0,-1):
+			if r[i][0] != r[i-1][1]:
+				if (n-(i-1))%2 == 0:
+					t[i-1].rotate(d)
+				elif (n-(i-1))%2 != 0:
+					t[i-1].rotate(-1*d)
+			elif r[i][0] == r[i-1][1]:
+				break
+                
+	# 오른쪽에 있는 톱니들 돌리기
+	if n != 3:
+		for i in range(n,3):
+			if r[i][1] != r[i+1][0]:
+				if (n-(i+1))%2 == 0:
+					t[i+1].rotate(d)
+				elif (n-(i+1))%2 != 0:
+					t[i+1].rotate(-1*d)
+			elif r[i][1] == r[i+1][0]:
+				break
+	t[n].rotate(d)
     
-    if b == 1: # 시계방향
-        graph[a-1]
-        
-    else: # 반시계
-        
-        
-        continue
+# 출력
+res = 0
+if t[0][0] == 1:
+	res+=1
+if t[1][0] == 1:
+	res+=2
+if t[2][0] == 1:
+	res+=4
+if t[3][0] ==1:
+	res+=8
+print(res)

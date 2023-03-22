3d2899c3a273a54f467380dfe122a5954c8e5761
enaa99 <wprkfdlsgk@gmail.com>
2023-03-22 13:32:19 +0900

    구현

--- dataStructure/스택.c
+++ dataStructure/스택.c
@@ -3,19 +3,18 @@
 typedef struct Node
 {
     int data;
-    struct Node* next;
-    
+    Node* next;
 }Node;
 
 typedef struct Stack
 {
-    struct Node* top;
-    /* data */
+    Node* top;
 }Stack;
 
 void init(Stack* s){
     s->top = NULL;
 }
+
 int isEmpty(Stack* s){
     return s->top == NULL;
 }
@@ -26,46 +25,21 @@ void push(Stack* s, int data){
     temp->next = s->top;
     s->top = temp;
 }
-
 int pop(Stack* s){
-    if(isEmpty(s)){
+    if (isEmpty(s))
         return -1;
-    }
-    else{
-        Node* temp = s->top;
-        int data = temp->data;
-        s->top = temp->next;
-        free(temp);
-        register data;
-    }
-}
-void print_data(Stack* s){
-    if(isEmpty(s)){
-        printf("Empty");
-    }
-    Node* temp = s->top;
-    for (temp; temp->next != NULL; temp = temp->next)
-    {
-        printf("data ->%d\n",temp->data);
-    }
-    
-}
 
+
+    Node* temp = s->top;
+    s->top = temp->next;
+    int data = temp->data;
+    free(temp);
+    register data;
+}
 
 
 
 int main(void){
-
-
-Stack s;
-init(&s);
-
-
-
-
-
-
-
 
     return 0;
 }

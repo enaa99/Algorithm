383ba5e8e1f17a9031475b6217e2385baa0074fc
enaa99 <wprkfdlsgk@gmail.com>
2023-04-03 17:25:12 +0900

    check sort

--- baekjoonPython/프로그래머스/특이한정렬.py
+++ baekjoonPython/프로그래머스/특이한정렬.py
@@ -6,3 +6,30 @@ def solution(numlist, n):
 
     
     return numlist
+
+
+
+
+
+
+
+def solution(k,n,a,b,A):
+    
+    isUsed = [0]*(b-a+1)
+    
+    for i in range(len(A)):
+        
+        if A[i] <= a:
+            while A[i] <= b:
+                if A[i] >= a and not isUsed[A[i]-a]:
+                    isUsed[A[i]-a] = 1
+                    break
+                A[i] += k
+        elif A[i] >= b:
+            while A[i] >= a:
+                if A[i] <=b and not isUsed[A[i]-b]:
+                    isUsed[A[i]-a] = 1
+                    break
+                A[i] -= k
+                
+	

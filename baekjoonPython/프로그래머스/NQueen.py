686f90fbb299671d741e4fbaa00c2506112b2019
enaa99 <wprkfdlsgk@gmail.com>
2023-05-31 23:20:30 +0900

    stack

--- baekjoonPython/프로그래머스/NQueen.py
+++ baekjoonPython/프로그래머스/NQueen.py
@@ -1,28 +1,16 @@
-def solution(n):
-    answer = 0
-    
-    arr = [-1]*n
-    
-    def check(x):
-        for i in range(x):
-            if arr[x] == arr[i] or abs(arr[x]-arr[i]) == abs(x-i):
-                return False
-        return True
-    
-    
-    
-    def DFS(depth):
-        nonlocal answer
-        if depth == n:
-            answer +=1
-            return
-        
-        for i in range(n):
-            arr[depth] = i
-            if check(depth):
-                DFS(depth+1)
-    DFS(0)
-    
-    return answer
 
-solution(12)
+def solution(n):
+    check_col = [False] * 100; check_d1 = [False] * 100; check_d2 = [False] * 100
+    def process(row):
+        answer = 0
+        if row == n+1:
+            return 1
+        for i in range(1,n+1):
+            d1 = row+i; d2 = n + (row - i)
+            if check_col[i] == False and check_d1[d1] == False and check_d2[d2] == False:
+                check_col[i] = True; check_d1[d1] = True; check_d2[d2] = True
+                answer += process(row+1)
+                check_col[i] = False; check_d1[d1] = False; check_d2[d2] = False
+        return answer
+    answer = process(1)
+    return answer

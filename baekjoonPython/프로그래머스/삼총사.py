7dbc59554cfeb847daec32c60b6351b67b5c274a
enaa99 <wprkfdlsgk@gmail.com>
2023-06-01 00:27:02 +0900

    backtracking

--- baekjoonPython/프로그래머스/삼총사.py
+++ baekjoonPython/프로그래머스/삼총사.py
@@ -19,14 +19,29 @@ def solution(number):
                 isUsed[i] = False
     
     dfs(0,0)
-    
     for a,b,c in check:
         if a+b+c == 0:
             answer +=1
-    
                 
     return answer
 
+
+def solution2(number):
+    answer = 0
+    
+    def DFS(depth,v,cnt):
+        nonlocal answer
+        if depth == 3:
+            if v == 0:
+                answer +=1
+            return
+        
+        for i in range(cnt,len(number)):
+            DFS(depth+1,v+number[i],i+1)
+    
+    DFS(0,0,0)
+    
+    return answer
 
 
 

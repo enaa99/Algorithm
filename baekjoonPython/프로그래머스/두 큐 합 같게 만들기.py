e4a67aa7c70e6c7f31fe8bc7552e337a93ba7489
enaa99 <wprkfdlsgk@gmail.com>
2023-06-04 19:26:14 +0900

    q의 개수

--- baekjoonPython/프로그래머스/두 큐 합 같게 만들기.py
+++ baekjoonPython/프로그래머스/두 큐 합 같게 만들기.py
@@ -37,4 +37,38 @@ def solution(queue1, queue2):
     
     
     
+
+def solution2(queue1, queue2):
+    answer = 0
     
+    queue1 = deque(queue1)
+    queue2 = deque(queue2)
+    
+    a = sum(queue1)
+    b = sum(queue2)
+    
+    check = len(queue1)*4
+    tmp = 0
+    
+    while True:
+        if a > b:
+            k = queue1.popleft()
+            queue2.append(k)
+            a -= k
+            b += k
+            answer +=1
+        elif a < b:
+            k = queue2.popleft()
+            queue2.append(k)
+            a += k
+            b -= k
+            answer +=1
+        else:
+            break
+        
+        tmp +=1
+        if tmp == check:
+            return -1
+    
+    
+    return answer
---
+++ baekjoonPython/프로그래머스/큐개수체크.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+from collections import deque
+def solution(q):
+    
+    answer = 0
+    counts = len(q)//3
+    q= deque(q)
+    
+    check = defaultdict(int)
+    
+    for i in q:
+        check[i] +=1
+    
+    while True:
+        if check[1] == check[2] == check[3]:
+            break
+        
+        answer +=1
+        check[q.popleft()] -=1
+        
+        
+        for i in check.keys():
+            if check[i] < counts:
+                check[i] +=1
+                q.append(i)
+                break
+        
+    return answer
+
+

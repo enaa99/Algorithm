207790b5ae67ed9904324c0d3cfe97dfa2d516f8
enaa99 <wprkfdlsgk@gmail.com>
2023-06-08 00:12:11 +0900

    dp

--- baekjoonPython/프로그래머스/멀리뛰기.py
+++ baekjoonPython/프로그래머스/멀리뛰기.py
@@ -9,5 +9,24 @@ def solution(n):
     
     for i in range(3,n+1):
         dp[i] = (dp[i-1] + dp[i-2])%1234567
+
+    return dp[n]
+
+
+def solution(n):
+    answer = 0
+    
+    dp = [0]*(n+4)
+    
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 3
+    dp[4] = 5
+    
+    for i in range(5,n+1):
+        dp[i] =(dp[i-2] + dp[i-1])%1234567
     
     return dp[n]
+
+
+    

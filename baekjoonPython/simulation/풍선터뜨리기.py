a101d0faa8d387f85653406c8afa4a5c5be514a4
enaa99 <wprkfdlsgk@gmail.com>
2023-08-15 12:18:33 +0900

    two pointer

---
+++ baekjoonPython/simulation/풍선터뜨리기.py
@@ -0,0 +1,31 @@
+def solution(a):
+    answer = 1
+    
+    min_a = min(a)
+    
+    pos = a.index(min_a)
+    
+    l = 0
+    r = len(a)-1
+    
+    min_l = a[l]
+    min_r = a[r]
+    
+    
+    while l<pos or r>pos:
+        l+=1
+        r-=1
+        
+        if l <= pos and a[l] < min_l:
+            answer +=1
+            min_l = a[l]
+        
+        if r >= pos and a[r] < min_r:
+            answer +=1
+            min_r = a[r]
+        
+        
+    
+    
+    return answer
+
--- pracC++/main.cpp
+++ pracC++/main.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
+#include <string>
 
-class A {
-  int data_;
-
- public:
-  A(int data) : data_(data) {}
-  void DoSomething(int x) const {
-    data_ = x;  // 불가능!
-  }
-
-  void PrintData() const { std::cout << "data: " << data_ << std::endl; }
-};
 
 int main() {
-  A a(10);
-  a.DoSomething(3);
-  a.PrintData();
+
+    std::string s = "abc";
+
+    std::cout << s << std::endl;
+
+    return 0;
+
 }

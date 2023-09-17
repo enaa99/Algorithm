fc3dac01aa835e9a2466ae1f939da9b40c889fb7
enaa99 <wprkfdlsgk@gmail.com>
2023-09-17 19:03:51 +0900

    leetcodee prac

---
+++ leetCode/26.Remove Diplicates from sorted array.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        s = set(nums)
+        nums.clear()
+
+        for i in s:
+            nums.append(i)
+        nums.sort()
+        return len(nums)
+
+        

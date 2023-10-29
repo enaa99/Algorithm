ade59677143952d615a41f154cb119739ceef478
enaa99 <wprkfdlsgk@gmail.com>
2023-10-29 17:25:29 +0900

    sliding window

--- programmers/subString.py
+++ programmers/subString.py
@@ -1,18 +1,17 @@
+from collections import deque
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        n = len(s)
+        
+        strings = deque()
+
         maxLength = 0
-        charSet = set()
-        left = 0
-        
-        for right in range(n):
-            if s[right] not in charSet:
-                charSet.add(s[right])
-                maxLength = max(maxLength, right - left + 1)
+        for char in s:
+            if char not in strings:
+                strings.append(char)
+                maxLength = max(maxLength, len(strings))
             else:
-                while s[right] in charSet:
-                    charSet.remove(s[left])
-                    left += 1
-                charSet.add(s[right])
-        
+                while char in strings:
+                    strings.popleft()
+                strings.append(char)
+
         return maxLength
